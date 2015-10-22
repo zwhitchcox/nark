2a30e735b602e899394d334c2be12ac245f67340
zane <zwhitchcox@gmail.com>
2015-10-21 20:29:44 -0400

    implement TDD

--- nark.js
+++ nark.js
@@ -6,30 +6,31 @@ const koa      = require('koa'),
 	thinky       = require('thinky'),
 	EventEmitter = require('events').EventEmitter,
 	gulp         = require('gulp'),
-	path         = require('path')
+	path         = require('path'),
+	serverCode = require('./server-code')
 
 module.exports = Nark
 
 function Nark(config) {
-	if (!(this instanceof Nark)) return new Nark()
+	if (!(this instanceof Nark)) return new Nark(config)
 	EventEmitter.call(this)
-	let self          = this
-		self.config     = config
-		self.app        = koa(),
-		self.router     = router(),
-		self.thinky     = thinky(self.config.rethinkdb)
-		self.serve      = serve,
-		self.sendfile   = sendfile,
-		self.serverCode = require('./server-code')
-		self.BASEPATH   = path.normalize(__dirname + '/../..')
+	let self        = this
+	self.config     = config
+	self.app        = koa()
+	self.router     = router()
+	self.thinky     = thinky(self.config.rethinkdb)
+	self.serve      = serve
+	self.sendfile   = sendfile
+	self.BASEPATH   = path.normalize(__dirname + '/../..')
 	// dependency inject nark into
 	// all server side js files (sjs)
+	// kind of ugly... I know ... :/
 	let stream = gulp
-		.src([self.BASEPATH+'/logic/{,**/}*.sjs'], {buffer:false})
-		.pipe(self.serverCode(nark))
+		.src(['logic/{,**/}*.sjs','!logic/{,**/}*spec.sjs'], {buffer:false})
+		.pipe(serverCode(self))
 		.pipe(gulp.dest('doesntdoanything'))
 	stream.on('end', () => {
-		nark.emit('built')
+		self.emit('built')
 	})
 }
 Nark.prototype = Object.create(EventEmitter.prototype)
